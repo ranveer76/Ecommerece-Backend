abbd4f0ea9c9b01e559d2cbc4b3bd7314fe43e13
Ranveer Singh Walia <ranveerwalia76@gmail.com>
2024-11-04 12:29:41 +0530

    increase timeout

--- database/db.js
+++ database/db.js
@@ -1,11 +1,17 @@
-require('dotenv').config()
-const mongoose=require("mongoose")
+require("dotenv").config();
+const mongoose = require("mongoose");
 
-exports.connectToDB=async()=>{
-    try {
-        await mongoose.connect(process.env.MONGO_URI)
-        console.log('connected to DB');
-    } catch (error) {
-        console.log(process.env.MONGO_URI);
-    }
-}
+exports.connectToDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 20000,
+      socketTimeoutMS: 45000,
+      maxPoolSize: 10,
+    });
+    console.log("connected to DB");
+  } catch (error) {
+    console.log("error connecting to DB", error.message);
+  }
+};
